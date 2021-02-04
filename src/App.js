6cea7245ea27eb66c4900323b841409d1e3eaea2
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-04 17:41:54 -0600

    React is up and working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Welcome to the scroll</h1>
+        <Route path="/post" exact render = {() => <Home />} />
+      </div>
     </div>
   );
 }
