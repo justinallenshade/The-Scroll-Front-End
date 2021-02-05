c1ab80b3723787ad23ad92aa4de68541407da44b
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-05 14:52:40 -0600

    fixed?

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 
-<<<<<<< HEAD
-function Home(props) {
-=======
 export default function Home(props) {
->>>>>>> login page base
     return (
         <div>
             <h1>Home</h1>
         </div>
     );
 }
-
-<<<<<<< HEAD
-export default Home;
-=======
->>>>>>> login page base
