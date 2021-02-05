f51ed1ba189e984d4c896a2bd58676110856c4b8
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-05 14:21:59 -0600

    login page base

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import Home from './Components/Home'
+import Login from './Components/Login'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div>
         <h1>Welcome to the scroll</h1>
         <Route path="/post" exact render = {() => <Home />} />
+        <Route path="/login" exact render = {() => <Login />} />
       </div>
     </div>
   );
