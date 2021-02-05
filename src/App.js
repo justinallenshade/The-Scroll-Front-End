0d0414613375e51198f5b3ae9f92adeb8651e3b1
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-05 14:40:47 -0600

    account create set up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import Home from './Components/Home'
-import Login from './Components/Login'
+import AccCreate from './Components/AccountCreate'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <div>
         <h1>Welcome to the scroll</h1>
         <Route path="/post" exact render = {() => <Home />} />
-        <Route path="/login" exact render = {() => <Login />} />
+        <Route path="/login" exact render = {() => <AccCreate />} />
       </div>
     </div>
   );
