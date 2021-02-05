3ec0de98271cbfccf18e084440f456e771f59d66
dorianl <dorian.lopez001@gmail.com>
2021-02-05 16:04:27 -0500

    worked on homepage design

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,27 @@ import React from "react";
 export default function Home(props) {
     return (
         <div>
-            <h1>Home</h1>
+            <div id="nav-bar">
+                <p>Header</p>
+                <p>Categories</p>
+                <p>URL:wlknevknve</p>
+            </div>
+
+            <div id="Main-container">
+
+                <div id="Aside">
+                    <h2>Aside</h2>
+                </div>
+
+                <div id="Timeline">
+                    <p>Post</p>
+                </div>
+
+                <div id="Followers">
+                <h2>Followers</h2>
+                </div>
+            </div>
+
         </div>
     );
 }
