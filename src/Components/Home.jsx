b49315bd822e0c28f70da09f847e2827093f61e3
dorianl <dorian.lopez001@gmail.com>
2021-02-06 19:05:55 -0500

    added bootsrap cdn with scrolling timeline

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,9 +15,40 @@ export default function Home(props) {
                     <h2>Aside</h2>
                 </div>
 
-                <div id="Timeline">
-                    <h2>Timeline</h2>
-                </div>
+                <div id="Timeline" class="list-group overflow-auto">
+                <a href="www.google.com" class="list-group-item list-group-item-action active" aria-current="true">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-5">List group item heading</h5>
+            <small>1 day ago</small>
+          </div>
+          <p class="mb-5">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small>Donec id elit non mi porta.</small>
+        </a>
+        <a href="www.google.com" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-5">List group item heading</h5>
+            <small class="text-muted">2 days ago</small>
+          </div>
+          <p class="mb-5">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small class="text-muted">Donec id elit non mi porta.</small>
+        </a>
+        <a href="www.google.com" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-5">List group item heading</h5>
+            <small class="text-muted">3 days ago</small>
+          </div>
+          <p class="mb-5">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small class="text-muted">Donec id elit non mi porta.</small>
+        </a>
+        <a href="www.google.com" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-5">List group item heading</h5>
+            <small class="text-muted">4 days ago</small>
+          </div>
+          <p class="mb-5">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small class="text-muted">Donec id elit non mi porta.</small>
+        </a>
+    </div>
 
                 <div id="Followers">
                 <h2>Followers</h2>
