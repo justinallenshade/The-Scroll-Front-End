d57adf0601e9837199196eb605bd346c9e3ad391
Martez McDaniel <martez@Martezs-MacBook-Air.local>
2021-02-08 09:12:17 -0600

    next step connect files

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 
 function Post({ post }) {
@@ -7,19 +6,19 @@ function Post({ post }) {
     "https://images.unsplash.com/photo-1612366630921-ec3bdea36f48?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fDZzTVZqVExTa2VRfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
   return (
     <Container>
-      <div class="card mb-3" style={{ maxwidth: 540 }}>
-        <div class="row g-0">
-          <div class="col-md-4">
+      <div className="card mb-3" style={{ maxwidth: 540 }}>
+        <div className="row g-0">
+          <div className="col-md-4">
             <img src={pic} alt="..." style={{ width: 275, height: 300 }} />
           </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">{post.title}</h5>
-              <p class="card-text">{post.body}</p>
-              <p class="card-text">{post.category}</p>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{post.title}</h5>
+              <p className="card-text">{post.body}</p>
+              <p className="card-text">{post.category}</p>
 
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
               </p>
             </div>
           </div>
