bac3c61c1d6645c75d3ebaaf1ab374fef6176d30
dorianl <dorian.lopez001@gmail.com>
2021-02-08 10:01:29 -0500

    Latest update to the home page

--- src/index.js
+++ src/index.js
@@ -5,17 +5,10 @@ import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-<<<<<<< HEAD
-=======
-import { BrowserRouter as Router } from 'react-router-dom';
-import InfiniScroll from './Components/InfiniScroll';
->>>>>>> added bootsrap cdn with scrolling timeline
-
 
 ReactDOM.render(
   <Router>
     <App />
-    <InfiniScroll />
   </Router>,
   document.getElementById("root")
 );
