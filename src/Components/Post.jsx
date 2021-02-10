be7e32fbe2a7cab5cce3cc49e764348821255772
Martez McDaniel <martez@Martezs-MacBook-Air.local>
2021-02-09 18:11:23 -0600

    first

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 import { Container } from "react";
->>>>>>> update to the front-end files
-=======
 import { Container } from "react";
->>>>>>> update to the front-end files
 
 function Post({ post }) {
  return (
@@ -24,4 +19,31 @@ function Post({ post }) {
 
   );
  }
+import TimeStamp from "./Timestamp";
+
+function Post({ post }) {
+  const pic =
+    "https://images.unsplash.com/photo-1612366630921-ec3bdea36f48?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fDZzTVZqVExTa2VRfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
+  return (
+    <div>
+      <div id="container">
+        <div>
+          <img id="user-image" src={pic} alt="user pic" />
+        </div>
+        <section>
+          <h3 className="card-title">Title:{post.title}</h3>
+          <h5 className="card-body">Body:{post.body}</h5>
+          <h6 className="card.category">Category:{post.category}</h6>
+          <div className="created-on">
+            <h6>Created on:</h6>
+            <section>
+              <TimeStamp />
+            </section>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
 export default Post;
