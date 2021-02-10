99f55e89dd751bec43ac729767d2a605adc891a8
rainydaysgameplays <rainydaysgameplays@gmail.com>
2021-02-10 00:29:45 -0600

    fixing loop of calls

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,23 +2,21 @@ import React, { useState, useEffect } from "react";
 import Posts from "./Posts"
 
 export default function Home(props) {
-  const [posts, setPosts] = useState([]);
+  let posts =[]
   let http = "https://the-scroll-back-end.herokuapp.com/post";
 
-  const getPosts = () => {
-    fetch(http)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        setPosts(res);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  useEffect(() => {
-    getPosts();
-  });
+  
+  fetch(http)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      posts = (res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
+
 
 
 
@@ -40,7 +38,7 @@ export default function Home(props) {
 
           <div id = "update-post">
             <h3 className="crud-heading">Edit post</h3>
-            <form action="https://the-scroll-back-end.herokuapp.com/post" method="put" id="postForm">
+            <form action="https://the-scroll-back-end.herokuapp.com/post?_method=PUT" method="post" id="postForm">
                 <input type="text" placeholder="id" name="id"></input>
                 <input type="text" placeholder="title" name="title"></input>
                 <input type="text" placeholder="body" name="body"></input>
@@ -57,21 +55,6 @@ export default function Home(props) {
                 <input type="submit" className='loginButton' value="Submit"></input>
             </form>
           </div> */}
-
-          <div id="blog-info">
-            <ul>
-              <li>Name: Ron Swanson</li>
-              <li>I do woodwork and drink lagavulin whiskey.</li>
-              <li>Location: Pawnee, IN</li>
-              <li>Joined: Jan, 10, 2021</li>
-            </ul>
-          </div>
-
-          <div id="follow-count">
-            <h3>Followers</h3>
-            <h3>Following</h3>
-            <h3>Posts</h3>
-          </div>
         </header>
 
         <div id="content-wrap">
